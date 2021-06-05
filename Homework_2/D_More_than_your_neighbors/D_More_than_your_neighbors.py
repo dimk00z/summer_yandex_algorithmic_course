7a15efc81245f71e20f73f4376d1682626bbf3f3
Dmitry <dimk00z@gmail.com>
2021-06-05 15:22:44 +0300

    HW2 D solution

---
+++ Homework_2/D_More_than_your_neighbors/D_More_than_your_neighbors.py
@@ -0,0 +1,18 @@
+def count_neighbors(number_list):
+    neighbors_count = 0
+    for element_position, element in enumerate(number_list[1:]):
+        if element_position+2 == len(number_list):
+            break
+        if (element > number_list[element_position]) \
+                and (element > number_list[element_position+2]):
+
+            neighbors_count += 1
+    return str(neighbors_count)
+
+
+with open('input.txt') as file:
+    number_list = list(map(int, file.read().split()))
+
+
+with open('output.txt', 'w') as file:
+    file.write(count_neighbors(number_list))

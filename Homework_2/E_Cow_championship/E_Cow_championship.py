1ef1f3b2392be3001d57497feb7701222afde083
Dmitry <dimk00z@gmail.com>
2021-06-07 11:37:45 +0300

    Update E_Cow_championship.py

--- Homework_2/E_Cow_championship/E_Cow_championship.py
+++ Homework_2/E_Cow_championship/E_Cow_championship.py
@@ -2,6 +2,10 @@
 10
 555 76 661 478 889 453 555 359 601 835
 555 подходит вроде (на 7 позиции)
+
+52
+645 535 575 155 965 805 700 115 875 805 485 465 75 955 275 835 105 115 75 325 435 975 695 965 985 745 115 65 95 525 445 715 815 85 755 365 685 935 945 195 775 465 335 475 585 425 45
+
 '''
 
 
@@ -12,22 +16,21 @@ def fetch_Vasya_position(champions_number, results):
     max_position = results.index(max_result)
     if ((champions_number-1)-max_position) < 2:
         return 0
-    for position, value in enumerate(results[max_position-1:]):
-        if (value % 10 == 5) and (max_position+position+1) < champions_number:
-            if results[max_position+position] < value:
-                Vasya_result = value
-                Vasya_position = position + max_position
-                print(Vasya_result, Vasya_position)
-    return 0
-    # Vasya_position = max_position+1
-    # Vasya_result = results[Vasya_position]
-    # print(max_result, Vasya_result)
+    Vasya_result = None
     # print(results)
-    # if (Vasya_result % 10) != 5 or results[Vasya_position+1] > Vasya_result:
-    #     return 0
-    # results = sorted(results,  reverse=True)
-    # print(results, len(results))
-    # return results.index(Vasya_result)+1
+    for position, value in enumerate(results[max_position:]):
+        # print(value)
+        if (value % 10 == 5) and (max_position+position+1) < champions_number:
+
+            if results[max_position+position] > results[max_position+position+1]:
+                Vasya_result = value
+                # print(str(Vasya_result) + '  !!')
+                break
+    if Vasya_result:
+        results = sorted(results,  reverse=True)
+        # print(results, len(results))
+        return results.index(Vasya_result)+1
+    return 0
 
 
 with open('input.txt') as file:

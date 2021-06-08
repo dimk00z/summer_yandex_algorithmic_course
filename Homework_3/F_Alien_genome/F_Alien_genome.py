54d3f48ca995851567f4e3e1dd4dea2a90af02c1
Dmitry <dimk00z@gmail.com>
2021-06-08 18:06:56 +0300

    HW 3 F solution

--- Homework_3/F_Alien_genome/F_Alien_genome.py
+++ Homework_3/F_Alien_genome/F_Alien_genome.py
@@ -5,21 +5,24 @@ HYAASSTKFMSSTLQENNEIDYDMTTQPJHOYMYFLTCRQTARJHGFILQYENKPRCQHMEUZKQFZTXJFDBIMJJEKW
 '''
 
 
-def get_coincidence(DNA_1, DNA_2):
-
+def get_coincidence(DNA_A, DNA_B):
+    set_b = set()
+    for position in range(len(DNA_B)-1):
+        set_b.add(DNA_B[position:position+2])
     count = 0
-    for position in range(len(lines[0])-2):
-        current_genome = lines[0][position:position+2]
-        count += DNA_2.count(current_genome)
+    for position in range(len(DNA_A)-1):
+        current_genome = DNA_A[position:position+2]
+        if current_genome in set_b:
+            count += 1
     return str(count)
 
 
 with open('input.txt') as file:
 
     lines = file.readlines()
-    DNA_1 = lines[0].strip()
-    DNA_2 = lines[1].strip()
+    DNA_A = lines[0].strip()
+    DNA_B = lines[1].strip()
 
 
 with open('output.txt', 'w') as file:
-    file.write(get_coincidence(DNA_1, DNA_2))
+    file.write(get_coincidence(DNA_A, DNA_B))

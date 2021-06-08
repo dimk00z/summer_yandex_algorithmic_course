4d4eddce27fdfe9e908b1d34781479d4f46d3c1f
Dmitry <dimk00z@gmail.com>
2021-06-08 10:16:50 +0300

    Update F_Alien_genome.py

--- Homework_3/F_Alien_genome/F_Alien_genome.py
+++ Homework_3/F_Alien_genome/F_Alien_genome.py
@@ -1,12 +1,25 @@
+'''
+WIVYKHBSZLOGOROTIPKLQJWUVXLEZPAGPCDUPUZIDRJTMSHVDUEALKOILER
+HYAASSTKFMSSTLQENNEIDYDMTTQPJHOYMYFLTCRQTARJHGFILQYENKPRCQHMEUZKQFZTXJFDBIMJJEKWMHFLKZBCUCCKYSGLVZ
+7
+'''
+
+
+def get_coincidence(DNA_1, DNA_2):
+
+    count = 0
+    for position in range(len(lines[0])-2):
+        current_genome = lines[0][position:position+2]
+        count += DNA_2.count(current_genome)
+    return str(count)
+
+
 with open('input.txt') as file:
 
     lines = file.readlines()
     DNA_1 = lines[0].strip()
     DNA_2 = lines[1].strip()
-    count = 0
-    for position in range(len(lines[0])-2):
-        current_genome = lines[0][position:position+2]
-        count += DNA_2.count(current_genome)
-    # print(count)
+
+
 with open('output.txt', 'w') as file:
-    file.write(str(count))
+    file.write(get_coincidence(DNA_1, DNA_2))

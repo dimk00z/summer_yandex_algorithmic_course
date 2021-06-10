e5672e81c6d6f5f1ed83782198a317da77b086a0
Dmitry <dimk00z@gmail.com>
2021-06-10 22:15:35 +0300

    Update H_Deciphering_the_Maya_Script.py

--- Homework_4/H_Deciphering_the_Maya_Script/H_Deciphering_the_Maya_Script.py
+++ Homework_4/H_Deciphering_the_Maya_Script/H_Deciphering_the_Maya_Script.py
@@ -1,9 +1,27 @@
 with open('input.txt') as file:
     lines = file.readlines()
 
-g, s = lines[0].split()
-letter_sequence = lines[1].strip()
+g, s = tuple(map(int, lines[0].split()))
+letter_sequence = set(lines[1].strip())
 word = lines[2].strip()
-# with open('output.txt', 'w') as file:
-#     file.write(check_errors(
-#         dictionary, sentence))
+counter = 0
+check_letter = 0
+previous_in = False
+difference = s-g
+
+for position, letter in enumerate(word):
+    if letter in letter_sequence:
+
+        if position < difference:
+            check = 1
+            for i in range(1, g+1):
+                next_letter = word[position+i]
+                if next_letter not in letter_sequence:
+                    break
+                check += 1
+                print(check)
+            if check == g:
+                counter += 1
+
+
+print(counter)

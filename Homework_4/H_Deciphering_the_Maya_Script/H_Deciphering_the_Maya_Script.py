b061eeced49ed39b4f51549fea24a18e1fe903b6
Dmitry <dimk00z@gmail.com>
2021-06-10 15:34:48 +0300

    HW 4 Init

---
+++ Homework_4/H_Deciphering_the_Maya_Script/H_Deciphering_the_Maya_Script.py
@@ -0,0 +1,9 @@
+with open('input.txt') as file:
+    lines = file.readlines()
+
+g, s = lines[0].split()
+letter_sequence = lines[1].strip()
+word = lines[2].strip()
+# with open('output.txt', 'w') as file:
+#     file.write(check_errors(
+#         dictionary, sentence))

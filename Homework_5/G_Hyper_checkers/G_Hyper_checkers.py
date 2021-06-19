0562afd862a88f81d41c71774bf59b13cfe881bd
Dmitry <dimk00z@gmail.com>
2021-06-19 15:59:39 +0300

    HW 5 G good for 5

--- Homework_5/G_Hyper_checkers/G_Hyper_checkers.py
+++ Homework_5/G_Hyper_checkers/G_Hyper_checkers.py
@@ -1,12 +1,39 @@
 from collections import Counter
+from itertools import permutations
 
 
 def calculate_variants(n, k, x):
     result = 0
-    cnt = Counter(n)
-    for element, element_sum in cnt.items():
-        if element_sum >= 3:
+    cnt = Counter(x)
+    # print(cnt)
+    # previous_element = None
+    for element in cnt:
+        if cnt[element] > 2:
             result += 1
+            cnt[element] = 2
+        high_border = element*k
+        cnt_with_border = {
+            element: element_sum
+            for element, element_sum in cnt.items()
+            if element <= high_border and element_sum != 0
+        }
+        inner_list = []
+        for inner_element in cnt_with_border:
+            if cnt_with_border[inner_element] > 2:
+                result += 1
+                cnt_with_border[inner_element] = 2
+            inner_list += [inner_element]*cnt_with_border[inner_element]
+        if len(inner_list) == 1:
+            continue
+        # print(inner_list)
+        inner_set = set(permutations(inner_list, 3))
+        result += len(inner_set)
+        # for inner_element in cnt_with_border:
+        #     if
+        cnt[element] = 0
+        # print(cnt_with_border)
+        # previous_element = element
+        # print(inner_set, len(inner_set))
     return result
 
 
---
+++ Homework_5/G_Hyper_checkers/test.py
@@ -0,0 +1,2 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))

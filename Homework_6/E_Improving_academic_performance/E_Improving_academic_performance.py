b94849ef0f50f177f5ed1df1767b96ca89803c95
Dmitry <dimk00z@gmail.com>
2021-06-19 17:26:53 +0300

    HW 6 E good for 32

---
+++ Homework_6/E_Improving_academic_performance/E_Improving_academic_performance.py
@@ -0,0 +1,23 @@
+def count(a, b, c):
+    sum = 2 * a + 3 * b + 4 * c
+    cnt = a + b + c
+    l = 0
+    r = cnt + 1
+    while l < r:
+        m = (l + r) // 2
+        if (sum + 5 * m) / (cnt + m) < 3.5:
+            l = m + 1
+        else:
+            r = m
+    return str(l)
+
+
+with open('input.txt') as file:
+    lines = file.readlines()
+    a = int(lines[0])
+    b = int(lines[1])
+    c = int(lines[2])
+
+
+with open('output.txt', 'w') as file:
+    file.write(count(a, b, c))

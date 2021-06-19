9f593b86f8ae0e8bfc2736b432579ce81760ca95
Dmitry <dimk00z@gmail.com>
2021-06-19 16:01:23 +0300

    Update G_Hyper_checkers.py

--- Homework_5/G_Hyper_checkers/G_Hyper_checkers.py
+++ Homework_5/G_Hyper_checkers/G_Hyper_checkers.py
@@ -5,8 +5,7 @@ from itertools import permutations
 def calculate_variants(n, k, x):
     result = 0
     cnt = Counter(x)
-    # print(cnt)
-    # previous_element = None
+
     for element in cnt:
         if cnt[element] > 2:
             result += 1
@@ -25,15 +24,11 @@ def calculate_variants(n, k, x):
             inner_list += [inner_element]*cnt_with_border[inner_element]
         if len(inner_list) == 1:
             continue
-        # print(inner_list)
         inner_set = set(permutations(inner_list, 3))
         result += len(inner_set)
-        # for inner_element in cnt_with_border:
-        #     if
+
         cnt[element] = 0
-        # print(cnt_with_border)
-        # previous_element = element
-        # print(inner_set, len(inner_set))
+
     return result
 
 

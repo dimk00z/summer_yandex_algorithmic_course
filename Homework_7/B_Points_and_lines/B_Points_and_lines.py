66b2dfa8d00156269e411f45b56210538144fd2a
Dmitry <dimk00z@gmail.com>
2021-06-22 23:17:29 +0300

    HW 7 B ML on 4 test

--- Homework_7/B_Points_and_lines/B_Points_and_lines.py
+++ Homework_7/B_Points_and_lines/B_Points_and_lines.py
@@ -1,14 +1,3 @@
-def calculate(n, m, lines, points):
-    # print(n, m, lines, points)
-    result = []
-    for point in points:
-        point_count = 0
-        for line in lines:
-            if min(line[0], line[1]) <= point and point <= max(line[0], line[1]):
-                point_count += 1
-        result.append(str(point_count))
-
-    return ' '.join(result)
 
 
 with open('input.txt') as file:
@@ -17,7 +6,20 @@ with open('input.txt') as file:
     n, m = map(int, lines[0].split())
     points = list(map(int, lines[-1].split()))
     lines = [tuple(map(int, line.split())) for line in lines[1:n+1]]
+result = ['']*m
+hole_line = []
+for line in lines:
+    hole_line.append((line[0], -1))
+    hole_line.append((line[1], 1))
+for point_position in range(m):
+    hole_line.append((points[point_position], 0, str(point_position)))
+hole_line.sort()
+count = 0
+for point in hole_line:
+    count += point[1]*-1
+    if point[1] == 0:
+        result[int(point[2])] = str(count)
 
 
 with open('output.txt', 'w') as file:
-    file.write(calculate(n, m, lines, points))
+    file.write(' '.join(result))

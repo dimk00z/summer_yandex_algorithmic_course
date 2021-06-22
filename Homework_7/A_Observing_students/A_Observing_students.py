57ec6a7ff9333c5c6c49c1b281d20097d15a2e89
Dmitry <dimk00z@gmail.com>
2021-06-22 21:27:33 +0300

    HW 7 A Solution

--- Homework_7/A_Observing_students/A_Observing_students.py
+++ Homework_7/A_Observing_students/A_Observing_students.py
@@ -1,15 +1,31 @@
-def do_observing(n, m, b_e):
-    students = {}
-    # print(students)
-    for teacher in b_e:
-        for teacher_look in range(teacher[0], teacher[1]+1):
-            if teacher_look not in students:
-                students[teacher_look] = True
+from functools import reduce
 
-    # print(students)
-    result = n - len(students)
+
+def combine_overlaps(ranges):
+    # https://coderoad.ru/15273693/Объединение-нескольких-диапазонов#59401068
+
+    return reduce(
+        lambda acc, el: acc[:-1:] + [(min(*acc[-1], *el), max(*acc[-1], *el))]
+        if acc[-1][1] >= el[0] - 1
+        else acc + [el],
+        ranges[1::],
+        ranges[0:1],
+    )
+
+
+def do_observing(n, m, b_e):
+
+    result = 0
+    result_intersection = combine_overlaps(sorted(b_e))
+    for intersection in result_intersection:
+        if intersection[1] == intersection[0]:
+            result += 1
+        else:
+            result += intersection[1]-intersection[0]+1
+    result = n - result
+    # print(result_intersection, result)
+
     return str(result)
-    # return str(students.count(None))
 
 
 with open('input.txt') as file:
@@ -17,7 +33,7 @@ with open('input.txt') as file:
     # n -students, m - teachers
     n, m = map(int, lines[0].split())
     # b_e - teachers
-    b_e = sorted([tuple(map(int, line.split())) for line in lines[1:]])
+    b_e = [tuple(map(int, line.split())) for line in lines[1:]]
 
 
 with open('output.txt', 'w') as file:

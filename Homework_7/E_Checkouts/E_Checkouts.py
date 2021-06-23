15fb6285d966b098ff27bf6d8ced76893aa7a08a
Dmitry <dimk00z@gmail.com>
2021-06-23 10:36:31 +0300

    HW 7 E init

---
+++ Homework_7/E_Checkouts/E_Checkouts.py
@@ -0,0 +1,32 @@
+def calculate_time(n, times):
+    events = []
+    count = 0
+    for time in times:
+        t1 = time[0]*60+time[1]
+        t2 = time[2]*60+time[3]
+        events.append((t1, 1))
+        events.append((t2, -1))
+        if t1 >= t2:
+            count += 1
+    ans = 0
+    start = 0
+    for x, type in sorted(events):
+        count += type
+        if count == n:
+            start = x
+        if count == n-1 and type == -1:
+            ans += x-start
+    if count == n:
+        ans += 24*60-start
+    print(ans)
+
+
+with open('input.txt') as file:
+    lines = file.readlines()
+    # n -checkouts, d - distance between students
+    n = int(lines[0].split())
+    times = [tuple(map(int, line.split())) for line in lines[1:]]
+del lines
+
+with open('output.txt', 'w') as file:
+    file.write(calculate_time(n, times))

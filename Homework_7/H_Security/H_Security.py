b8321d1c35a23325d0f600d6d7a6de535aa7ab80
Dmitry <dimk00z@gmail.com>
2021-06-23 17:52:16 +0300

    HW 7 init

---
+++ Homework_7/H_Security/H_Security.py
@@ -0,0 +1,32 @@
+def check_security(full_security):
+    result = []
+    for security in full_security:
+        count = 0
+        for point, type in security:
+            count += type
+            # print(point, type, count)
+            if count > 1:
+                result.append('Wrong Answer')
+                break
+        else:
+            result.append('Accepted')
+        # print(count)
+    return '\n'.join(result)
+
+
+with open('input.txt') as file:
+    lines = file.readlines()
+    k = int(lines[0])
+    full_security = []
+    for line in lines[1:]:
+        security = []
+        for t1 in line.split()[1::2]:
+            security.append((t1, 1))
+        for t2 in line.split()[2::2]:
+            security.append((t2, -1))
+        security.sort()
+        full_security.append(security)
+    # print(full_security)
+
+with open('output.txt', 'w') as file:
+    file.write(check_security(full_security))

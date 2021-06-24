eec19c5cb1974e80e3a020c86c5558dfa5099c86
Dmitry <dimk00z@gmail.com>
2021-06-24 16:44:52 +0300

    HW 8 C Solution

---
+++ Homework_8/C_The_second_maximum/C_The_second_maximum.py
@@ -0,0 +1,82 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            if data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+
+        else:
+            self.data = data
+
+    def get_max_node(self, root):
+        result = []
+        if root:
+            if root.right:
+                result = self.get_max_node(root.right)
+            else:
+                result = result+self.get_max_node(root.left)
+            result.append(root.data)
+        result.sort()
+        return result
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data)
+        if self.right:
+            self.right.print_tree()
+
+    def get_node_from_tree(self, node, data, depth=0):
+        depth += 1
+        if not data:
+            return None
+        if data == node.data:
+            return node, depth
+        if data < node.data:
+            if node.left:
+                return self.get_node_from_tree(node.left, data, depth)
+        if data > node.data:
+            if node.right:
+                return self.get_node_from_tree(node.right, data, depth)
+
+    def __str__(self):
+        return str(self.data)
+
+
+def max_depth(node):
+
+    if node is None:
+        return 0
+    left_depth = max_depth(node.left)
+    right_depth = max_depth(node.right)
+
+    if left_depth > right_depth:
+        return left_depth+1
+    else:
+        return right_depth+1
+
+
+with open('input.txt') as file:
+    nodes = tuple(map(int, file.readlines()[0].split()))
+root = Node(nodes[0])
+result = []
+for node in nodes[1:-1]:
+    root.insert(node)
+
+result = root.get_max_node(root)[-2]
+
+# print(result)
+with open('output.txt', 'w') as file:
+    file.write(str(result))

6e08b8236eea34c22890c043ee0b08d1ac5fd293
Dmitry <dimk00z@gmail.com>
2021-06-24 11:25:27 +0300

    HW 7 C solution

--- Homework_7/C_Seating_in_the_classroom/C_Seating_in_the_classroom.py
+++ Homework_7/C_Seating_in_the_classroom/C_Seating_in_the_classroom.py
@@ -1,35 +1,43 @@
-def calculate_variants(n, d, x):
-    line_with_distance = []
-    final_students = [0]*n
-    for student_position, student in enumerate(x):
-        line_with_distance.append(
-            (student - d, -1))
-        line_with_distance.append(
-            (student, 0, str(student_position)))
-        line_with_distance.append(
-            (student + d, 1))
-    line_with_distance.sort()
+# Thx https://github.com/Yankovsky/yandex-algos-training/blob/master/hw7/c.py
 
-    count = 0
-    last_count = 0
-    current_variant = 0
+from heapq import heappop, heappush
+
+STUDENT_START = -1
+STUDENT_END = 1
+
+
+def calculate_variants(n, d, x):
+
+    line_with_distance = []
+    max_student = 0
+    for student in x:
+        max_student = max(max_student, student)
+        line_with_distance.append(
+            (student, STUDENT_START))
+        line_with_distance.append(
+            (student + d, STUDENT_END))
+    line_with_distance.sort()
+    # print(line_with_distance)
+
+    final_students = {}
+    heap = list(range(1, n + 1))
+    max_exam_number = 0
 
     for point in line_with_distance:
-        count += point[1]*-1
-        if count == 0:
-            current_variant = 0
+        if point[1] == STUDENT_START:
+            # print(heap)
+            next_exam_number = heappop(heap)
+            max_exam_number = max(max_exam_number, next_exam_number)
+            final_students[point[0]] = next_exam_number
 
-        if point[1] == 0:
-            if current_variant == 0:
-                current_variant = 1
-            final_students[int(point[2])] = current_variant
-            # print(point[0], point[2], current_variant)
-            current_variant += 1
-        last_count = count
+        elif point[1] == STUDENT_END:
+            student_exam_number = final_students[point[0] - d]
+            heappush(heap, student_exam_number)
 
     result = []
-    result.append(str(max(final_students)))
-    result.append(' '.join(map(str, final_students)))
+    result.append(str(max_exam_number))
+    result.append(
+        ' '.join(map(str, [final_students[student] for student in x])))
     return '\n'.join(result)
 
 

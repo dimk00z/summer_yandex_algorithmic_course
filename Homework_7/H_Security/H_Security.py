7f4a5dfbd899dc8cca4c07e9ef487b529347c1e0
Dmitry <dimk00z@gmail.com>
2021-07-13 15:59:47 +0300

    HW 7 H solution from video

--- Homework_7/H_Security/H_Security.py
+++ Homework_7/H_Security/H_Security.py
@@ -1,56 +1,41 @@
-def check_security(full_security):
-    result = []
-    for security in full_security:
-        count = 0
-        security.sort()
-        print()
-        print(security)
-        last_count = 0
-        if len(security) == 2 and security[0][0] != 0 and security[1][0] != 10000:
-            result.append('Wrong Answer')
-            continue
-        if security[-1][0] != 10000 or security[0][0] != 0:
-            result.append('Wrong Answer')
-            continue
-        for position, current_security in enumerate(security):
-            point, type = current_security
-            count += type
-            print(position, count, point, type)
-            # print(point, type, count)
-            if count == 0 and position != len(security)-1:
-                if current_security[0] != security[position+1][0]:
-                    # print(current_security, security[position+1])
-                    result.append('Wrong Answer')
-                    break
-            print('last_count ', last_count, 'count ',
-                  count, last_count > count, count-1 > 0)
-            # print('count ', count)
-            # if last_count > count and count-1 > 0:
-            #     result.append('Wrong Answer')
-            #     break
-            if count > 2:
-                result.append('Wrong Answer')
+def check_security(k, lines):
+    result = ['']*k
+    for test_number, line in enumerate(lines):
+        nums = list(map(int, line.split()))
+        n = nums[0]
+        events = [0]*(2*n)
+        for i in range(1, len(nums), 2):
+            events[i-1] = (nums[i], -1, i)
+            events[i] = (nums[i+1], 1, i)
+        events.sort()
+        goodseq = set()
+        nowseq = set()
+        goodflag = True
+        prevtime = -1
+        for event in events:
+            if event[0] != 0 and len(nowseq) == 0:
+                goodflag = False
                 break
-
-            last_count = count
+            if len(nowseq) == 1 and event[0] != prevtime:
+                goodseq.update(nowseq)
+            if event[1] == -1:
+                nowseq.add(event[2])
+            else:
+                nowseq.remove(event[2])
+            prevtime = event[0]
+        if events[-1][0] != 10000:
+            goodflag = False
+        if goodflag and len(goodseq) == n:
+            result[test_number] = 'Accepted'
         else:
-            result.append('Accepted')
-        # print(count)
+            result[test_number] = 'Wrong Answer'
     return '\n'.join(result)
 
 
 with open('input.txt') as file:
     lines = file.readlines()
     k = int(lines[0])
-    full_security = []
-    for line in lines[1:]:
-        security = []
-        for t1 in line.split()[1::2]:
-            security.append((int(t1), 1))
-        for t2 in line.split()[2::2]:
-            security.append((int(t2), -1))
-        full_security.append(security)
-    # print(full_security)
+
 
 with open('output.txt', 'w') as file:
-    file.write(check_security(full_security))
+    file.write(check_security(k, lines[1:]))
